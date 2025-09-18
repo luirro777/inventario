e01f806d3bb8af9a945158342806ad2e896062b4
Luis Romano <luisromano@gmail.com>
2025-09-18 20:04:08 -0300

    Agregado admin

--- inventario/productos/admin.py
+++ inventario/productos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'precio', 'stock', 'necesita_reposicion']
+    list_filter = ['stock']
+    search_fields = ['nombre']
